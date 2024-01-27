08249ff6505408e67710a1f8506c36ef2ee36826
Manojkumar886 <maddymano434@gmail.com>
2024-01-27 17:59:47 +0530

    Srithar Front end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { MyfirstFunction } from "./Sample";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <table cellPadding={'4px'} border={'3px'}>
+                <thead>
+                    <tr>
+                        <th>S.no</th>
+                        <th>Name</th>
+                        <th>Age</th>
+                    </tr>
+                </thead>
+            </table >
+            <MyfirstFunction />
+        </>
+    )
 }
+
 
 export default App;
---
+++ src/Sample.js
@@ -0,0 +1,13 @@
+function MyfirstFunction() {
+    return (
+        <>
+            <h1>WHAT IS REACT JS?</h1>
+            <p>React is a JavaScript library for building user interfaces.
+                React is not a framework.its a liabrary.
+                Data flow is always in a single direction.
+                React is used to build single-page applications.</p>
+        </>
+    )
+}
+
+export { MyfirstFunction };
